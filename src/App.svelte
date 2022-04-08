2576f8a4c6902e7d097f77b8dc0fe01fd4e40090
Emil Rapp <emilemail4@gmail.com>
2022-04-08 11:52:44 +0200

    basic code for showing keys

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,65 @@
 <script>
-	export let name;
+  import Key from "./components/Key.svelte";
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+  <h1>Rapple!</h1>
+  <div class="keyboard-row">
+    <Key letter="Q" />
+    <Key letter="W" />
+    <Key letter="E" />
+    <Key letter="R" />
+    <Key letter="T" />
+    <Key letter="Y" />
+    <Key letter="U" />
+    <Key letter="I" />
+    <Key letter="O" />
+    <Key letter="P" />
+    <Key letter="Å" />
+  </div>
+  <div class="keyboard-row">
+    <Key letter="A" />
+    <Key letter="S" />
+    <Key letter="D" />
+    <Key letter="F" />
+    <Key letter="G" />
+    <Key letter="H" />
+    <Key letter="J" />
+    <Key letter="K" />
+    <Key letter="L" />
+    <Key letter="Ö" />
+    <Key letter="Ä" />
+  </div>
+  <div class="keyboard-row">
+    <Key letter="Z" />
+    <Key letter="X" />
+    <Key letter="C" />
+    <Key letter="V" />
+    <Key letter="B" />
+    <Key letter="N" />
+    <Key letter="M" />
+  </div>
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 360px;
+    margin: 0 auto;
+  }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
+  .keyboard-row {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    align-items: center;
+    margin: 0 auto;
+  }
 </style>
---
+++ src/components/Key.svelte
@@ -0,0 +1,22 @@
+<script>
+  export let letter = "A";
+  export let letterColor = "rgb(203, 203, 203)";
+
+  function ooops() {
+    alert(letter);
+  }
+</script>
+
+<main>
+  <div on:click={ooops}>
+    <p>{letter}</p>
+  </div>
+</main>
+
+<style>
+  div {
+    background-color: rgb(203, 203, 203);
+    padding: 0 10px 0 10px;
+    margin: 0 auto;
+  }
+</style>
