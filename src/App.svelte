c26f5307488755d4d92ed9164908a96aa61050cc
Emil Rapp <emilemail4@gmail.com>
2022-04-13 09:34:43 +0200

    småsaker

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,6 @@
 <script>
   import Key from "./components/Key.svelte";
+  import Letterbox from "./components/Letterbox.svelte";
   let keys = "QWERTYUIOPÅASDFGHJKLÖÄZXCVBNM";
 </script>
 
@@ -7,7 +8,11 @@
   <div class="titlebar">
     <h1 class="title">Rapple!</h1>
   </div>
-  <div class="boxholder" />
+  <div class="boxholder">
+    {#each { length: 30 } as _, i}
+      <Letterbox />
+    {/each}
+  </div>
   <div class="keyboard">
     {#each keys as key}
       <Key letter={key} />
@@ -18,7 +23,8 @@
 <style>
   main {
     text-align: center;
-    max-width: 420px;
+    width: 420px;
+    max-height: 100%;
     margin: 0 auto;
     display: flex;
     flex-direction: column;
@@ -30,9 +36,9 @@
     width: 100%;
   }
   .title {
-    color: #ffa061;
+    color: white;
     text-transform: uppercase;
-    font-size: 4em;
+    font-size: 3em;
     font-weight: 100;
     margin: 0 auto;
   }
@@ -42,5 +48,14 @@
     justify-content: center;
     align-items: center;
     width: 100%;
+  }
+  .boxholder {
+    display: grid;
+    flex-grow: 1;
+    gap: 10px;
+    grid-template-columns: repeat(5, 1fr);
+    grid-template-rows: repeat(6, 1fr);
+    width: 80%;
+    margin-bottom: 30px;
   }
 </style>
--- src/components/Letterbox.svelte
+++ src/components/Letterbox.svelte
@@ -3,6 +3,19 @@
   export let color = "#1c1c1c";
 </script>
 
-<main />
+<main>
+  <p>{letter}</p>
+</main>
 
-<style></style>
+<style>
+  main {
+    background-color: #334155;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 58px;
+    height: 58px;
+    font-size: 2rem;
+    border-radius: 0.5rem;
+  }
+</style>
